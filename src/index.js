d6eea531e10d4655590be7c5705c11fb4148e348
devispeed <devispeed.2003@gmail.com>
2025-11-17 06:18:40 +0100

    product page is done

--- src/index.js
+++ src/index.js
@@ -16,4 +16,27 @@ document.querySelectorAll(".add-to-cart-btn").forEach((item) => {
   });
 });
 
-document.getElementById('copyright').innerHTML = "جميع الحقوق محفوضة سنة " + new Date().getFullYear()
+document
+  .querySelectorAll('.size-option input[type="radio"]')
+  .forEach((item) => {
+    item.addEventListener("change", () => {
+      document.querySelectorAll(".size-option").forEach((i) => {
+        i.classList.remove("active");
+      });
+      item.parentNode.parentNode.classList.add("active");
+    });
+  });
+
+  document
+  .querySelectorAll('.color-option input[type="radio"]')
+  .forEach((item) => {
+    item.addEventListener("change", () => {
+      document.querySelectorAll(".color-option").forEach((i) => {
+        i.classList.remove("active");
+      });
+      item.parentNode.parentNode.classList.add("active");
+    });
+  });
+
+document.getElementById("copyright").innerHTML =
+  "جميع الحقوق محفوضة سنة " + new Date().getFullYear();
